1a96f94eebdd8e8b6b7a73fed5a82a8c15aaccf1
Caglar <caglarsulun@hotmail.com>
2021-12-22 03:56:54 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function handleClick() {
+    console.log(`Button was clicked`);
+  }
+
+  return (
+    <>
+      <button
+        onFocus={(e) => {
+          console.log("Good!");
+        }}
+        onBlur={(e) => {
+          console.log("Hey! Eyes on me!");
+        }}
+        placeholder="focus and blur"
+        onClick={() => handleClick(1)}
+      >
+        Eyes on me
+      </button>
+    </>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  function handleChange(event) {
+    console.log("Entering password...");
+    console.log(`${event.target.name}: ${event.target.value}`);
+  }
+
+  return (
+    <>
+      <input type="password" onChange={handleChange} />
+    </>
+  );
+}
+
+export default Keypad;
